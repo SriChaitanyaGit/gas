5dcef70e40f11b111e6b44125fef80e76183de01
SriChaitanyaGit <gnanasrichaitanya2004@gmail.com>
2025-07-17 11:35:53 +0530

    project setup

---
+++ src/test/java/cfg/lms/gas/GasApplicationTests.java
@@ -0,0 +1,13 @@
+package cfg.lms.gas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
